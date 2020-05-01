f66c617f34dde329f24adb51adad445c8546dcda
wangkath <56166245+wangkath@users.noreply.github.com>
2020-05-01 00:37:24 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    const [text, changeText] = useState("");
+    
     return(
         <div>
             <h1>Test your typing speed!</h1>
-            <textarea />
+            <textarea 
+              value = {text} 
+              onChange = {(event) => changeText(event.target.value)}
+            />
             <h4>time remaining: </h4>
             <button>Start Game!</button>
             <h1>Word Count: </h1>
+            <h1>{text}</h1>
         </div>
     )
 }
