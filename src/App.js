94cf797e958302f20deb228c60b56393332ce323
wangkath <56166245+wangkath@users.noreply.github.com>
2020-05-04 10:07:01 -0700

    select time

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, {useState, useEffect, useRef} from 'react'
 import './App.css'
 
 function App() {
-    const STARTING_TIME = 5
     const inputRef = useRef(null)
 
+    const [startingTime, setStartingTime] = useState(null)
     const [text, changeText] = useState("")
     const [words, changeWords] = useState(0)
-    const [time, changeTime] = useState(STARTING_TIME)
+    const [time, changeTime] = useState(startingTime)
     const [start, changeStart] = useState(false)
+    const [WPM, changeWPM] = useState(0)
 
     function handleChange(event) {
       const {value} = event.target
@@ -24,40 +25,60 @@ function App() {
     }
 
     function startGame() {
-      changeStart(true)
-      changeTime(STARTING_TIME)
-      changeWords(0)
-      changeText("")
-      inputRef.current.disabled = false
-      inputRef.current.focus()
+      if(startingTime > 0) {
+        changeTime(startingTime)
+        changeStart(true)
+        changeWords(0)
+        changeText("")
+        inputRef.current.disabled = false
+        inputRef.current.focus()
+      } else {
+        alert('select a time')
+      }
     }
 
     function endGame() {
       changeStart(false)
       count(text)
+      changeWPM(60*words/startingTime)
+    }
+
+    function updateTime(e) {
+      const {value} = e.target
+      setStartingTime(value)
+      changeTime(value)
     }
 
     useEffect(() => {
       if(start && time > 0) {
         const timer = setTimeout(() => {changeTime(time => time-1)},1000)
-      } else {
+      } else if(time === 0) {
         endGame()
       }
     },[time,start])
     
     return(
         <div>
-            <h1>Test your typing speed!</h1>
+            <h1>Test your typing speed! Choose time and press start to begin:)</h1>
+            <select disabled = {start} value = {startingTime} onChange = {updateTime}>
+              <option value = "">select time</option>
+              <option value = {5}>5 seconds</option>
+              <option value = {10}>10 seconds</option>
+              <option value = {30}>30 seconds</option>
+              <option value = {60}>1 minute</option>
+              <option value = {120}>2 minutes</option>
+            </select>
+            <h5>selected time: {startingTime > 0 ? `${startingTime} seconds`: "please select a time"}</h5>
+            <h4 className = "timeRem">time remaining: {startingTime > 0 ? `${time} seconds`: "please select a time"}</h4>
             <textarea 
               ref = {inputRef} 
               value = {text} 
               onChange = {handleChange}
               disabled = {!start}
             />
-            <h4>time remaining: {time}</h4>
             <button onClick = {startGame} disabled = {start}>Start Game!</button>
             <h1>Word Count: {words}</h1>
-            <h1>Typing Speed: {60*words/STARTING_TIME} words per minute</h1>
+            <h1>Typing Speed: {WPM} words per minute</h1>
         </div>
     )
 }
